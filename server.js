5ed77ae7a1619082e17d66feb7fc5b9094c7017e
Jay Savaliya <jrsavaliya04@gmail.com>
2024-06-29 16:16:24 +0530

    CHg: try to solve origin error no = 1

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const suggestionRoute = require('./Routes/suggestion.route');
 var cookieParser = require('cookie-parser');
 
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production' ? 'https://inter-view-client.vercel.app' : 'http://localhost:4000',
+    origin: 'https://inter-view-client.vercel.app',
     credentials: true
 }));
 app.use(cookieParser());
